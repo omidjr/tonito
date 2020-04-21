68bc1b2efc954231ca9e13680c907733d037b412
Omid Jahandar <omid.jahandar.98@gmail.com>
2020-04-21 16:29:21 +0430

    Added Skeleton effect to Menu

--- src/components/Menu/Drinks.jsx
+++ src/components/Menu/Drinks.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid, Typography, Button } from "@material-ui/core";
+import Skeleton from "@material-ui/lab/Skeleton";
 import { useStyles, FeatureImage } from "../Styles/Styles";
 import { FoodData, formatPrice } from "../Data/FoodData";
 
@@ -12,68 +13,98 @@ export const Drinks = () => {
         food.item === "Drinks" ? (
           <Grid item xs={12} sm={6} md={4}>
             <div className={classes.menu}>
-              <FeatureImage img={food.img} />
-              <Typography variant="h4" className={classes.menuLabel}>
-                {food.name}
-              </Typography>
-              <div className={classes.sizes}>
-                <hr className={classes.bar} />
-                <Typography variant="h6" style={{ padding: "0px 25px" }}>
-                  Pick Size
+              {food.img ? (
+                <FeatureImage img={food.img} />
+              ) : (
+                <Skeleton
+                  style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+                  variant="rect"
+                  height={250}
+                />
+              )}
+              {food.img ? (
+                <Typography variant="h4" className={classes.menuLabel}>
+                  {food.name}
                 </Typography>
-                <hr className={classes.bar} />
-              </div>
-              <div
-                className={classes.sizes}
-                style={{
-                  padding: "10px 20px",
-                  marginBottom: "0px",
-                  justifyContent: "space-between",
-                }}
-              >
-                <div
+              ) : (
+                <Skeleton
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    flexDirection: "column",
+                    backgroundColor: "rgba(0, 0, 0, 0.2)",
+                    margin: "30px 10% 0px 10%",
+                    padding: "8px 0px",
                   }}
-                >
-                  <Button
-                    className={classes.cart}
-                    variant="contained"
-                    style={{ borderRadius: "10%", fontSize: "1rem" }}
+                />
+              )}
+              {food.img ? (
+                <>
+                  <div className={classes.sizes}>
+                    <hr className={classes.bar} />
+                    <Typography variant="h6" style={{ padding: "0px 25px" }}>
+                      Pick Size
+                    </Typography>
+                    <hr className={classes.bar} />
+                  </div>
+                  <div
+                    className={classes.sizes}
+                    style={{
+                      padding: "10px 20px",
+                      marginBottom: "0px",
+                      justifyContent: "space-between",
+                    }}
                   >
-                    Single
-                  </Button>
-                  <Typography
-                    style={{ padding: "5px 0px 0px 19%" }}
-                    variant="subtitle1"
-                  >
-                    {formatPrice(food.price1)}
-                  </Typography>
-                </div>
-                <div
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <Button
+                        className={classes.cart}
+                        variant="contained"
+                        style={{ borderRadius: "10%", fontSize: "1rem" }}
+                      >
+                        Single
+                      </Button>
+                      <Typography
+                        style={{ padding: "5px 0px 0px 19%" }}
+                        variant="subtitle1"
+                      >
+                        {formatPrice(food.price1)}
+                      </Typography>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <Button
+                        className={classes.cart}
+                        variant="contained"
+                        style={{ borderRadius: "10%", fontSize: "1rem" }}
+                      >
+                        Family
+                      </Button>
+                      <Typography
+                        style={{ padding: "5px 0px 0px 25%" }}
+                        variant="subtitle1"
+                      >
+                        {formatPrice(food.price2)}
+                      </Typography>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <Skeleton
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    flexDirection: "column",
+                    backgroundColor: "rgba(0, 0, 0, 0.2)",
+                    margin: "5px 10% 0px 10%",
+                    padding: "60px 0px",
                   }}
-                >
-                  <Button
-                    className={classes.cart}
-                    variant="contained"
-                    style={{ borderRadius: "10%", fontSize: "1rem" }}
-                  >
-                    Family
-                  </Button>
-                  <Typography
-                    style={{ padding: "5px 0px 0px 25%" }}
-                    variant="subtitle1"
-                  >
-                    {formatPrice(food.price2)}
-                  </Typography>
-                </div>
-              </div>
+                />
+              )}
             </div>
           </Grid>
         ) : null
--- src/components/Menu/PizzaSets.jsx
+++ src/components/Menu/PizzaSets.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Typography, Button, Grid } from "@material-ui/core";
+import Skeleton from "@material-ui/lab/Skeleton";
 import { useStyles, FeatureImage } from "../Styles/Styles";
 import { FoodData, formatPrice } from "../Data/FoodData";
 
@@ -12,75 +13,115 @@ export const PizzaSets = () => {
         food.item === "Pizza" ? (
           <Grid item xs={12} sm={6} md={4}>
             <div className={classes.menu}>
-              <FeatureImage img={food.img} />
-              <Typography variant="h4" className={classes.menuLabel}>
-                {food.name}
-              </Typography>
-              <Typography
-                variant="h6"
-                className={classes.menuLabel}
-                style={{ paddingTop: "25px" }}
-              >
-                Extra olive oil, garlic, mozzarella, mushrooms and olives.
-              </Typography>
-              <div className={classes.sizes}>
-                <hr className={classes.bar} />
-                <Typography variant="h6" style={{ padding: "0px 25px" }}>
-                  Pick Size
+              {food.img ? (
+                <FeatureImage img={food.img} />
+              ) : (
+                <Skeleton
+                  style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+                  variant="rect"
+                  height={250}
+                />
+              )}
+              {food.img ? (
+                <Typography variant="h4" className={classes.menuLabel}>
+                  {food.name}
                 </Typography>
-                <hr className={classes.bar} />
-              </div>
-              <div
-                className={classes.sizes}
-                style={{
-                  padding: "10px 20px",
-                  marginBottom: "0px",
-                  justifyContent: "space-between",
-                }}
-              >
-                <div
+              ) : (
+                <Skeleton
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    flexDirection: "column",
+                    backgroundColor: "rgba(0, 0, 0, 0.2)",
+                    margin: "30px 10% 0px 10%",
+                    padding: "8px 0px",
                   }}
+                />
+              )}
+              {food.img ? (
+                <Typography
+                  variant="h6"
+                  className={classes.menuLabel}
+                  style={{ paddingTop: "25px" }}
                 >
-                  <Button
-                    className={classes.cart}
-                    variant="contained"
-                    style={{ borderRadius: "10%", fontSize: "1rem" }}
-                  >
-                    22cm
-                  </Button>
-                  <Typography
-                    style={{ padding: "5px 0px 0px 19%" }}
-                    variant="subtitle1"
-                  >
-                    {formatPrice(food.price1)}
-                  </Typography>
-                </div>
-                <div
+                  Extra olive oil, garlic, mozzarella, mushrooms and olives.
+                </Typography>
+              ) : (
+                <Skeleton
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    flexDirection: "column",
+                    backgroundColor: "rgba(0, 0, 0, 0.2)",
+                    margin: "-1px 10% 0px 10%",
+                    padding: "33px 0px",
                   }}
-                >
-                  <Button
-                    className={classes.cart}
-                    variant="contained"
-                    style={{ borderRadius: "10%", fontSize: "1rem" }}
+                />
+              )}
+              {food.img ? (
+                <>
+                  <div className={classes.sizes}>
+                    <hr className={classes.bar} />
+                    <Typography variant="h6" style={{ padding: "0px 25px" }}>
+                      Pick Size
+                    </Typography>
+                    <hr className={classes.bar} />
+                  </div>
+                  <div
+                    className={classes.sizes}
+                    style={{
+                      padding: "10px 20px",
+                      marginBottom: "0px",
+                      justifyContent: "space-between",
+                    }}
                   >
-                    29cm
-                  </Button>
-                  <Typography
-                    style={{ padding: "5px 0px 0px 19%" }}
-                    variant="subtitle1"
-                  >
-                    {formatPrice(food.price2)}
-                  </Typography>
-                </div>
-              </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <Button
+                        className={classes.cart}
+                        variant="contained"
+                        style={{ borderRadius: "10%", fontSize: "1rem" }}
+                      >
+                        22cm
+                      </Button>
+                      <Typography
+                        style={{ padding: "5px 0px 0px 19%" }}
+                        variant="subtitle1"
+                      >
+                        {formatPrice(food.price1)}
+                      </Typography>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        flexDirection: "column",
+                      }}
+                    >
+                      <Button
+                        className={classes.cart}
+                        variant="contained"
+                        style={{ borderRadius: "10%", fontSize: "1rem" }}
+                      >
+                        29cm
+                      </Button>
+                      <Typography
+                        style={{ padding: "5px 0px 0px 19%" }}
+                        variant="subtitle1"
+                      >
+                        {formatPrice(food.price2)}
+                      </Typography>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <Skeleton
+                  style={{
+                    backgroundColor: "rgba(0, 0, 0, 0.2)",
+                    margin: "-1px 10% 0px 10%",
+                    padding: "63px 0px",
+                  }}
+                />
+              )}
             </div>
           </Grid>
         ) : null
