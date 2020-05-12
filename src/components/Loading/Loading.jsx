46e7ada8c13df7cfe0d6ba61c5a64f2c6c7b6a66
Omid Jahandar <omid.jahandar.98@gmail.com>
2020-05-12 13:21:11 +0430

    Updated Loading section and img folder

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,20 +4,46 @@ import { Typography } from "@material-ui/core";
 
 export const Loading = () => {
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Typography variant="h2" style={{ color: "#ef4123" }}>
-        Tonito
-      </Typography>
-      <ReactLoading color="#ef4123" />
-    </div>
+    <>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          background: "url(/img/other/showcase2.jpg) no-repeat center / cover",
+        }}
+      >
+        <div
+          style={{
+            overflow: "hidden",
+            height: "100vh",
+            width: "100vw",
+            position: "absolute",
+            top: "0",
+            left: "0",
+            background: "#f92500",
+            zIndex: "1",
+            opacity: "0.77",
+          }}
+        />
+        <div
+          style={{
+            zIndex: "2",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Typography variant="h1" style={{ color: "#fff" }}>
+            Tonito
+          </Typography>
+          <ReactLoading width="58px" height="58px" />
+        </div>
+      </div>
+    </>
   );
 };
