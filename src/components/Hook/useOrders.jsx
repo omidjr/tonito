b21b412572faac9771636dd3fd409a9ef58f16da
Omid Jahandar <omid.jahandar.98@gmail.com>
2020-05-22 13:09:02 +0430

    Setup localStorage

--- src/components/Hook/useOrders.jsx
+++ src/components/Hook/useOrders.jsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const useOrders = () => {
-  const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState(() => {
+    const localData = localStorage.getItem("orders");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("orders", JSON.stringify(orders));
+  }, [orders]);
 
   return {
     orders,
